cca0d745f362786e683bd413896b6050bbcd7fcd
Anugya  gupta <anugyagupta043@gmail.com>
2025-05-10 11:04:50 +0530

    commit

--- netlify/functions/fetch-news.js
+++ netlify/functions/fetch-news.js
@@ -1,7 +1,10 @@
+// Using the API key directly since we can't use environment variables in free tier
+const NEWS_API_KEY = '24eeee565f1a4e1983a310c56c20fcfb';
+
 exports.handler = async function(event, context) {
   try {
     const response = await fetch(
-      `https://newsapi.org/v2/everything?q=fashion&language=en&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`
+      `https://newsapi.org/v2/everything?q=fashion&language=en&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
     );
     
     const data = await response.json();
